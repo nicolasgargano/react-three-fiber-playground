150bdef8dc9c7f5f24e56c828a14f579735de16e
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-08 23:55:06 -0300

    feat(boids-test): clamp velocity system

--- src/boids-test/systems/ClampVelocitySystem.tsx
+++ src/boids-test/systems/ClampVelocitySystem.tsx
@@ -4,13 +4,13 @@ import { Acceleration, Boid, Velocity } from '../facets'
 import { Vector3 } from 'three/src/math/Vector3'
 import { settings } from '../settings'
 
-export const ResetAccelerationSystem: FC = () => {
-    const query = useQuery(e => e.hasAll(Boid, Acceleration))
+export const ClampVelocitySystem: FC = () => {
+    const query = useQuery(e => e.hasAll(Boid, Velocity))
     const zero = new Vector3(0,0,0)
 
     return useSystem(_ => {
-        query.loop([Boid, Acceleration], (e, [_, {acceleration}]) => {
-            acceleration.copy(zero)
+        query.loop([Boid, Velocity, Velocity], (e, [_, {velocity}]) => {
+            velocity.clampLength(settings.minSpeed, settings.maxSpeed)
         })
     })
 }
