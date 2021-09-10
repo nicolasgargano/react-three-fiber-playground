12d6b0d49248d65fc2aa54a5994615b8b42cdf38
Nicolás Gargano <nicolasgargano@gmail.com>
2021-09-10 15:16:40 -0300

    feat(stars): change star focus

--- src/spring-test/StarsScene.tsx
+++ src/spring-test/StarsScene.tsx
@@ -1,12 +1,12 @@
 import React, { createContext, FC, MutableRefObject, useCallback, useContext, useRef, useState } from 'react'
-import { OrbitControls, Stars } from '@react-three/drei'
-import { EffectComposer, Outline } from '@react-three/postprocessing'
+import { OrbitControls, OrbitControlsProps, Stars, useCamera } from '@react-three/drei'
+import { EffectComposer, Outline} from '@react-three/postprocessing'
 import { Object3D } from 'three'
-import { MeshProps } from '@react-three/fiber'
+import { Vector3 } from 'three/src/math/Vector3'
+import { useSpring } from '@react-spring/three'
+import { MeshProps, useThree } from '@react-three/fiber'
 
-export type StarProps = {
-
-}
+export type StarProps = MeshProps
 
 const hoveredObjectsContext = createContext<React.Dispatch<React.SetStateAction<React.MutableRefObject<Object3D>[]>>>(() => [])
 
@@ -35,25 +35,88 @@ export const Star : FC<StarProps> = ({...props}) => {
 
 export const StarsScene = () => {
     const [hovered, setHovered] = useState<MutableRefObject<Object3D>[]>([])
+    const [target, setTarget] = useState<[number,number,number]>([0,0,0])
+
+    const [v, set] = useState(new Vector3(0,0,0))
+    const [starPositions] = useState([new Vector3(-5, 0, 0), new Vector3(5, 0, 0)])
+
+    const { gl, camera } = useThree()
+
+    type CameraAnimationProps = {
+        lookAt: [number, number, number],
+        position: [number, number, number]
+    }
+
+    const [cameraProps, api] = useSpring<CameraAnimationProps>(() => ({
+        reset: true,
+        from: {
+            lookAt: [0, 0, 0],
+            position: [0, 10, 10]
+        },
+        to: {
+            lookAt: [0, 0, 0],
+            position: [0, 10, 10]
+        }
+    }))
+
+    const [controlsEnabled, setControlsEnabled] = useState(true)
+
+    const focusStar = (vec3: Vector3) => {
+        const offsetFromCurrentTarget = camera.position.clone().sub(new Vector3(target[0], target[1], target[2]))
+
+        console.log('focus star')
+        api.start({
+            reset: true,
+            from: {
+                lookAt: target,
+                position: camera.position.toArray()
+            },
+            to: {
+                lookAt: vec3.toArray(),
+                position: vec3.clone().add(offsetFromCurrentTarget).toArray()
+            },
+            onStart: () => {
+                console.log('On start')
+                setControlsEnabled(() => false)
+            },
+            onRest: () => {
+                console.log('On rest')
+                setTarget(vec3.toArray())
+                setControlsEnabled(() => true)
+            },
+            onChange: (result) => {
+                console.log('on change')
+                camera.position.set(result.value.position[0], result.value.position[1], result.value.position[2])
+                camera.lookAt(result.value.lookAt[0], result.value.lookAt[1], result.value.lookAt[2])
+            }
+        })
+    }
+
 
     return <>
         <ambientLight intensity={0.1} />
         <directionalLight />
         <color attach="background" args={[0,0,0]} />
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+        <OrbitControls enabled={controlsEnabled} target={target} enablePan={true} enableZoom={true} enableRotate={true} />
 
         <Stars
             radius={100} // Radius of the inner sphere (default=100)
             depth={50} // Depth of area where stars should fit (default=50)
-            count={5000} // Amount of stars (default=5000)
+            count={500} // Amount of stars (default=5000)
             factor={4} // Size factor (default=4)
             saturation={0} // Saturation 0-1 (default=0)
             fade // Faded dots (default=false)
         />
 
         <hoveredObjectsContext.Provider value={setHovered}>
-            <Star/>
-
+            {
+                starPositions.map((pos, i) =>
+                    <Star
+                        key={`star-${i}`}
+                        position={pos}
+                        onClick={_ => focusStar(pos)}
+                    />)
+            }
         </hoveredObjectsContext.Provider>
 
         <EffectComposer multisampling={8} autoClear={false}>
